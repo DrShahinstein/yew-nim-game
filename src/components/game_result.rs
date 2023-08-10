3a73e5b9a0149b8dbb0fd38144f8c88fa2174f7f
DrShahinstein <1taylanozturk@protonmail.com>
2023-08-11 00:12:50 +0300

    Build up a colorful interface for result screen

--- src/components/game_result.rs
+++ src/components/game_result.rs
@@ -9,11 +9,16 @@ pub struct GameResultProps {
 #[function_component(GameResult)]
 pub fn game_result(props: &GameResultProps) -> Html {
     html! {
-        <div>
-            {match props.winner {
-                Player::User => "user won",
-                Player::Computer => "computer won",
-            }}
+        <div class="flex flex-col items-center justify-center h-screen bg-gradient-to-b from-purple-500 to-blue-600">
+            <h2 class="text-4xl mb-6 text-white">
+                {format!("{} Won!", match props.winner {
+                    Player::User => "You",
+                    Player::Computer => "Computer",
+                })}
+            </h2>
+            <button class="bg-gradient-to-r from-yellow-300 to-yellow-500 hover:from-yellow-400 hover:to-yellow-600 text-gray-800 font-bold py-3 px-6 rounded-full shadow-lg transform hover:scale-105 transition-transform duration-300">
+                {"Play Again"}
+            </button>
         </div>
     }
 }
